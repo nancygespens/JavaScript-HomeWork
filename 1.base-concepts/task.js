b650afd05ea8d5ff6ad8a1143fe59b4fca48eb77
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2022-11-29 11:57:52 +0300

    Задание №1 выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let d = Math.pow(b, 2) - 4 * a * c;
+    if (d === 0) {
+        arr = [(b * (-1)) / (2 * a)];
+    }
+    else if (d > 0) {
+        arr = [((b * (-1)) + Math.sqrt(d)) / (2 * a), ((b * (-1)) - Math.sqrt(d)) / (2 * a)];
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+        return false;
+    }
+    let convertedPercent = percent / 100;
+    let monthlyPercent = convertedPercent / 12;
+    let loanBody = amount - contribution;
+    let monthlyPay = loanBody * (monthlyPercent + (monthlyPercent / (Math.pow((1 + monthlyPercent), countMonths) - 1)));
+    let totalMortgage = Number((monthlyPay * countMonths).toFixed(2));
+    return totalMortgage;
 }
