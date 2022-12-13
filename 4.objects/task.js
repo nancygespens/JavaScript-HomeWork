b4d46f0126c9dae23534aab5f1832526366ceb1b
Anastasia Telesnitckaya <108212488+nancygespens@users.noreply.github.com>
2022-12-13 11:35:40 +0300

    Задание №4 выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+ }
+ 
+ let student1 = new Student("Albedo", "male", 20);
+ let student2 = new Student("Rosaria", "female", 29);
+ 
+ 
+ Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName; 
+   }
+ 
+ Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+       return undefined;
+    }
+   this.marks.push(...marks);
+ }
+ 
+ Student.prototype.getAverage = function () {
+   if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+   }
+   let avg = this.marks.reduce((acc, marc) => acc + marc, 0) / this.marks.length;
+   return avg;
+ }
+ 
+ Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
